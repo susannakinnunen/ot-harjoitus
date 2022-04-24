9fab24f208c140dcf9540a32b6ac597802ba8766
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2022-04-24 17:38:38 +0300

    new tests

--- src/tests/bodypart_test.py
+++ src/tests/bodypart_test.py
@@ -1,16 +1,14 @@
 import unittest
-import bodyparts
+from repositories.bodypart_repository import BodypartRepository
 from initialize_database import initialize_database
-import database_connection
 
 
 class TestBodyparts(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.bodypart_repository = BodypartRepository()
+        initialize_database()
 
     def test_add_bodypart(self):
-        initialize_database()
-        B = bodyparts.Bodypart(database_connection.get_database_connection())
-        vastaus = B.add_bodypart("niska")
+        vastaus = self.bodypart_repository.add_bodypart("niska")
 
         self.assertEqual(vastaus, "Lisätty kehonosa: niska")
--- src/tests/user_test.py
+++ src/tests/user_test.py
@@ -1,35 +1,34 @@
 import unittest
-import users
 from initialize_database import initialize_database
-import database_connection
+from repositories.user_repository import UserRepository
 
 
 class TestUser(unittest.TestCase):
     def setUp(self):
         initialize_database()
-        self.U = users.User(database_connection.get_database_connection())
+        self.user_repository = UserRepository()
         self.username = "testinimi"
         self.password = "testisalasana"
 
     def test_registering(self):
-        vastaus = self.U.add_user(self.username, self.password)
+        vastaus = self.user_repository.add_user(self.username, self.password)
         self.assertEqual(
             vastaus, "Lisätty käyttäjätunnus testinimi ja salasana testisalasana")
 
     def test_login_correct(self):
-        self.U.add_user(self.username, self.password)
-        vastaus = self.U.login(self.username, self.password)
+        self.user_repository.add_user(self.username, self.password)
+        vastaus = self.user_repository.login(self.username, self.password)
 
         self.assertEqual(vastaus, True)
 
     def test_login_incorrect_username(self):
-        self.U.add_user(self.username, self.password)
-        vastaus = self.U.login("vääränimi", self.password)
+        self.user_repository.add_user(self.username, self.password)
+        vastaus = self.user_repository.login("vääränimi", self.password)
 
         self.assertEqual(vastaus, False)
 
     def test_login_incorrect_password(self):
-        self.U.add_user(self.username, self.password)
-        vastaus = self.U.login(self.username, "vääräsalasana")
+        self.user_repository.add_user(self.username, self.password)
+        vastaus = self.user_repository.login(self.username, "vääräsalasana")
 
         self.assertEqual(vastaus, False)
